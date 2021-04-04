34e154ce9b58c6f3314ba7db035fe0fb43672a45
Jacob Kriese <jkriese12@gmail.com>
2021-04-04 09:24:33 -0700

    API Call updated to get pertinent data, showing as needed in console

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,6 +1,28 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
-
+import API from "./utils/API";
 function App() {
+  // Setting state for employees
+  const [employees, setEmployees] = useState([]);
+  // Calling API to initialize on page load to have all the data available
+  useEffect(() => {
+    API.search().then((res) => {
+      setEmployees(
+        // console.log(res.data.results);
+        res.data.results.map((emp) => {
+          return {
+            name: emp.name.first + " " + emp.name.last,
+            pic: emp.picture.thumbnail,
+            email: emp.email,
+            phone: emp.cell,
+            location: emp.location.city + ", " + emp.location.state,
+          };
+        })
+      );
+    });
+  }, []);
+
+  console.log(employees);
   return <div>hi</div>;
 }
 
--- employee-directory/src/utils/API.js
+++ employee-directory/src/utils/API.js
@@ -1,13 +1,9 @@
 import axios from "axios";
-
-const BASEURL = "https://api.giphy.com/v1/gifs/search?q=";
-const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=20";
-
-const marvelUrl =
-  "http://gateway.marvel.com/v1/public/characters?ts=1&limit=50&apikey=3c14f22cccc40f2a5fe8c84b010ca86c&hash=6fac2ceafd3cb12c55fe7b17df6ff4d6";
-// Export an object with a "search" method that searches the Giphy API for the passed query
+// API link to call from
+const randomApi = "https://randomuser.me/api/?results=100&nat=us";
+// Exporting function to get data from API
 export default {
-  search: function (query) {
-    return axios.get(marvelUrl);
+  search: function () {
+    return axios.get(randomApi);
   },
 };
